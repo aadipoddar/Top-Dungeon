7ce2699431e460acfae29fc8bfd64e253f95c2f5
Aadi Poddar <aadipoddarmail@gmail.com>
2022-03-04 19:34:29 +0530

    Manual Collision Detection

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -8,6 +8,8 @@ public class Player : MonoBehaviour
 
     private Vector3 moveDelta;
 
+    private RaycastHit2D hit;
+
     private void Start()
     {
         boxCollider = GetComponent<BoxCollider2D>();
@@ -22,12 +24,20 @@ public class Player : MonoBehaviour
         moveDelta = new Vector3(x, y, 0);
 
         // Swap sprite direction, whether you're going right or left
-        if(moveDelta.x > 0)
+        if (moveDelta.x > 0)
             transform.localScale = Vector3.one;
         else if (moveDelta.x < 0)
             transform.localScale = new Vector3(-1, 1, 1);
 
-        // Make this thing move!
-        transform.Translate(moveDelta * Time.deltaTime);
+        // Make sure we can move in this direction, by casting a box there first, if the box returns null, we're free to move
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        if (hit.collider == null)
+            // Make this thing move!
+            transform.Translate(0, moveDelta.y * Time.deltaTime, 0);
+
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x, 0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        if (hit.collider == null)
+            // Make this thing move!
+            transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
     }
 }
